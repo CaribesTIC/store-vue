aafba7ba20dd8dc92e74184478d54dee840596d4
Ernesto Canquiz <fdsoil123@gmail.com>
2023-03-12 19:35:00 -0400

    feat: Insert and update product

--- src/modules/Product/composables/Product/useTabProduct.ts
+++ src/modules/Product/composables/Product/useTabProduct.ts
@@ -6,7 +6,7 @@ import type { Product } from "../../types/Product";
 
 export default (productId?: string) => {
 
-  const { updateMeasureUnit } = inject<{
+  const { measureUnit, updateMeasureUnit } = inject<{
     updateMeasureUnit: (val: any) => void;
   }>('measureUnit')
 
@@ -50,10 +50,11 @@ export default (productId?: string) => {
 
   const insertProduct = async (product: Product) => {
     pending.value = true
+    product.measure_unit = 'measureUnit'
     return ProductService.insertProduct(product)
       .then((response) => {         
         alert( response.data.message )
-        router.push( { path: '/products' } )
+        router.push( { path: `/products/edit/${response.data.product_id}` } )
       })
       .catch((err) => {                
         console.log( err.response.data )
@@ -67,6 +68,7 @@ export default (productId?: string) => {
   const updateProduct = async (product: Product, productId: string) => {
     pending.value= true
     product._method = 'PUT'
+    product.measure_unit = 'measureUnit'    
     return ProductService.updateProduct(productId, product)
       .then((response) => {
         alert( response.data.message )
