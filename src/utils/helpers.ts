136d573dc8a36cf61d71c4393bfbb7d6bc510c39
Ernesto Canquiz <fdsoil123@gmail.com>
2023-07-13 20:41:00 -0400

    Function helper.getError

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -6,21 +6,45 @@ export const ascBubble = function (a, b) {
 }
 
 export const getError = (error) => {
-  const errorMessage = "API Error, please try again.";
+  const errorMessage = "API Error, please try again.";  
+  if (import.meta.env.DEV) {
+    console.error(error.response.headers ?? "No Headers")    
+    console.error("Status:", error.response.status)
+    console.error("Message:", error.message)
+    console.error(errorMessage)
+    console.log(error)  
+    if (!error.response.data) {
+      console.error(
+        "There is no Data.",
+        `API ${error.config.baseURL}${error.config.url} not found`
+      ) 
+    } else {
+      console.error('Data', error.response.data)
+      return error.response.data.errors ?? {}
+    }
+  }  
+  return errorMessage;
+};
 
+
+/*
+export const getError = (error) => {
+  const errorMessage = "API Error, please try again.";
   if (!error.response.data) {
     console.error(
       `API ${error.config.baseURL}${error.config.url} not found`
     );    
   }
-  if (import.meta.env.DEV) {     
-    console.error(error.response.data ?? "There is no Data");    
-    console.error(error.response.headers?? "No Headers");
-    console.error(errorMessage);
-    console.error("Status", error.response.status);      
+  if (import.meta.env.DEV) {
+    console.error(error.response.data ?? "There is no Data")
+    console.error(error.response.headers?? "No Headers")
+    console.error(errorMessage)
+    console.error("Status", error.response.status)
+    console.log(error)     
   }
   if (error.response.data && error.response.data.errors) {
     return error.response.data.errors;
   }
   return errorMessage;
 };
+*/
