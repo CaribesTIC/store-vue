b5c04d556f27e159f642705ddcfb42be70ddd9bb
Ernesto Canquiz <caribestic123@gmail.com>
2025-02-11 20:10:07 -0400

    feat: Customize environment variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,30 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  // uncomment these lines to containerize
-  // server: { 
-  //   host: '0.0.0.0',
-  //   port: 80
-  //}, 
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+    return {
+      server: { 
+        host: env.VITE_HOST,
+        port: env.VITE_PORT as unknown as number
+        }, 
+        plugins: [vue()],
+        resolve: {
+          alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+          }
+        },
+        define: {
+            'process.env': {                    
+              VITE_APP_API_URL: `${env.VITE_APP_API_URL}`
+            }
+        },
+        test: {
+            globals: true,
+            environment: "jsdom",
+        },
     }
-  },
-  define: {
-    'process.env': {
-      // comment this line to containerize
-      VUE_APP_API_URL: "http://localhost:8000",
-      // uncomment this line to containerize
-      // VUE_APP_API_URL: "http://api.store.dev.com"
-    }
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-  },
 })
